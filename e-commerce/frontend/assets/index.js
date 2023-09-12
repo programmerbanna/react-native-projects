12c7a77bb42207c2323eb188f06e0fd07845ac64
Hasanul Haque Banna <programmerbanna@gmail.com>
2023-09-12 11:14:46 +0600

    component added

---
+++ e-commerce/frontend/assets/index.js
@@ -0,0 +1,5 @@
+export { default as Brand } from "./brand.png";
+export { default as ScreenOne } from "./screen1.png";
+export { default as ScreenTwo } from "./screen2.png";
+export { default as ScreenThree } from "./screen3.png";
+export { default as Cart } from "./cart.png";
